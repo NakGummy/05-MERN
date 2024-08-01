c9a73da84808b21d3a97732bce7e1b6ecb7f27eb
miatia1025 <miatia1025@gmail.com>
2024-08-01 19:13:50 +0900

    keep: empty in users admin page

---
+++ frontend/src/pages/Admin/UserList.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import { FaTrash, FaEdit, FaCheck, FaTimes } from "react-icons/fa";
+import Loader from "../../components/Loader";
+import { toast } from "react-toastify";
+import {
+  useGetUsersQuery,
+  useDeleteUserMutation,
+  useUpdateUserMutation,
+} from "../../redux/api/usersApiSlice";
+import Message from "../../components/Message";
+
+const UserList = () => {
+  const { data: users, refetch, isLoading, error } = useGetUsersQuery();
+  const [deleteUser] = useDeleteUserMutation();
+  const [updateUser] = useUpdateUserMutation();
+
+  const [editableUserId, setEditableUserId] = useState(null);
+  const [editableUserName, setEditableUserName] = useState("");
+  const [editableUserEmail, setEditableUserEmail] = useState("");
+
+  useEffect(() => {
+    refetch();
+  }, [refetch]);
+
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-semibold mb-4">Users</h1>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">
+          {error?.data.message || error.message}
+        </Message>
+      ) : (
+        <div className="flex flex-col md:flex-row">
+          {/* Admin Menu */}
+          <table className="w-full md:w-4/5 mx-auto">
+            <thead>
+              <tr>
+                <th className="px-4 py-2 text-left">ID</th>
+                <th className="px-4 py-2 text-left">NAME</th>
+                <th className="px-4 py-2 text-left">EMAIL</th>
+                <th className="px-4 py-2 text-left">ADMIN</th>
+                <th className="px-4 py-2 text-left"></th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UserList;
